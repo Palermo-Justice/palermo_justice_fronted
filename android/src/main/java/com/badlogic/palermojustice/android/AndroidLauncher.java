a90c97eb139e89621a49a620ae435606156a3c33
Niklas Lang <niklas.lang01@gmail.com>
2025-04-04 13:07:48 +0200

    Add firebase service

--- android/src/main/java/com/badlogic/palermojustice/android/AndroidLauncher.java
+++ android/src/main/java/com/badlogic/palermojustice/android/AndroidLauncher.java
@@ -1,16 +1,33 @@
 package com.badlogic.palermojustice.android;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.badlogic.palermojustice.Main;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import android.util.Log;
 
 /** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
+    private FirebaseAnalytics firebaseAnalytics;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Initialize Firebase
+        FirebaseApp.initializeApp(this);
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        // Log an event to Firebase
+        Bundle bundle = new Bundle();
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "test_event");
+        firebaseAnalytics.logEvent("app_open", bundle);
+
+        Log.d("FirebaseTest", "Firebase initialized and test event sent.");
+
+        // LibGDX Initialization
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
         configuration.useImmersiveMode = true; // Recommended, but not required.
         initialize(new Main(), configuration);
