74c9bcecbf60dfd4ab3c3caf7d6f579db4e191bb
Niklas Lang <niklas.lang01@gmail.com>
2025-04-09 13:47:33 +0200

    Add firebase analytics capabilities

--- android/src/main/java/com/badlogic/palermojustice/android/AndroidLauncher.java
+++ android/src/main/java/com/badlogic/palermojustice/android/AndroidLauncher.java
@@ -7,15 +7,24 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.badlogic.palermojustice.Main;
 import com.badlogic.palermojustice.firebase.AndroidFirebaseService;
 import com.badlogic.palermojustice.firebase.FirebaseInterface;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 /** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
+    private FirebaseAnalytics firebaseAnalytics;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Create Firebase implementation for Android
         FirebaseInterface firebaseService = new AndroidFirebaseService();
+        //firebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        // Log an event to Firebase
+        //Bundle bundle = new Bundle();
+        //bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "test_event");
+        //firebaseAnalytics.logEvent("app_open", bundle);
 
         // Configure and initialize the application
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
