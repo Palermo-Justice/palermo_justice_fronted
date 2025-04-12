1ed84d7c91f0d417b411e87e58b2db14ea8c4149
jyztintan <justintanwk2001@gmail.com>
2025-04-12 16:39:51 +0200

    Implement Announcement Screen
    note that the constructor requires a input text for the announcement `

---
+++ core/src/main/kotlin/com/badlogic/palermojustice/view/AnnouncementScreen.kt
@@ -0,0 +1,64 @@
+package com.badlogic.palermojustice.view
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.utils.Align
+import com.badlogic.gdx.utils.viewport.ScreenViewport
+
+//!!note that a result text input needs to be passed
+class AnnouncementScreen(private val resultText: String) : Screen {
+    private lateinit var stage: Stage
+    private lateinit var skin: Skin
+
+    override fun show() {
+        stage = Stage(ScreenViewport())
+        Gdx.input.inputProcessor = stage
+        skin = Skin(Gdx.files.internal("pj2.json"))
+        createUI()
+    }
+
+    private fun createUI() {
+        val table = Table()
+        table.setFillParent(true)
+        stage.addActor(table)
+
+        // Title
+        val titleLabel = Label("Morning has come...", skin, "narration")
+        titleLabel.setAlignment(Align.center)
+        titleLabel.setFontScale(5f)
+        table.add(titleLabel).expandX().align(Align.center).padBottom(30f).row()
+
+        // Result Text
+        val resultLabel = Label(resultText, skin, "big")
+        resultLabel.setAlignment(Align.center)
+        resultLabel.setFontScale(2f)
+        table.add(resultLabel).expandX().align(Align.center).padBottom(30f).row()
+    }
+
+    override fun render(delta: Float) {
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        stage.act(delta)
+        stage.draw()
+    }
+
+    override fun resize(width: Int, height: Int) {
+        stage.viewport.update(width, height, true)
+    }
+
+    override fun pause() {}
+    override fun resume() {}
+    override fun hide() {}
+    override fun dispose() {
+        stage.dispose()
+        skin.dispose()
+    }
+}
